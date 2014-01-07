ee0577ef5db7c8f8d2ab29c0eb7494bbaf03abaa
Anthony Zhang <azhang102@gmail.com>
2014-01-07 01:45:02 -0500

    No tips venue page, no cat icon state, started new tip modal

--- js/main.js
+++ js/main.js
@@ -54,4 +54,58 @@ $(document).ready(function() {
 	$(document).on("click", ".header .logo", function(){
 		window.location.href = "/";
 	});
+	$(document).on("click", ".write-tip", function(){
+		tipModal();
+	});
 });
+
+/***********************************
+		HOME MADE MODAL RECIPE
+***********************************/
+
+function tipModal(){
+	var venue_id = getUrlVars()["id"];
+	var modalContainer = $("<div>", {id: "modalContainer"});
+	var dialog = $("<div>", {id: "dialog"});
+	
+	var newTip = '<h1>Write a #LiveTip</h1><form accept-charset="UTF-8" autocomplete="off" target="_self" action="http://temp-pro-tip.herokuapp.com/tips" class="new_tip" id="new_tip" method="post">';
+	newTip += '<input name="utf8" type="hidden" value="✓"><input name="authenticity_token" type="hidden" value="y1VyNJ5b4SFH4TnAwJqXgGYgYuNU4YT4cEIxCf/03qo=">';
+	newTip += '<input id="tip_venue_id" type="hidden" name="tip[venue_id]" value="' + venue_id + '">';
+	newTip += '<input id="tip_content" name="tip[content]" size="30" type="text">';
+  	newTip += '<input name="commit" type="submit" value="Create"></form>'
+
+  	dialog.html(newTip);
+  	dialog.append("<div class='close-dialog'></div>");
+  	$(modalContainer).appendTo("body");
+  	$(dialog).appendTo("body");
+
+	var maskHeight = $(document).height();
+	$("#modalContainer").css("height", maskHeight);
+	
+	$("#modalContainer").fadeIn(300);
+	$("#modalContainer").fadeTo("slow", 0.75);
+	$("#modalContainer").click(removeModal);
+	$("#dialog .close-dialog").click(removeModal);
+	$("#dialog").css("top", (($(window).height() - $("#dialog").outerHeight()) / 2) + $(window).scrollTop() + "px");
+	$("#dialog").fadeIn(500);
+	
+	$(window).resize(function(){
+		$("#dialog").css("top", (($(window).height() - $("#dialog").outerHeight()) / 2) + $(window).scrollTop() + "px");
+	})
+	$(window).scroll(function(){
+		$("#dialog").css("top", (($(window).height() - $("#dialog").outerHeight()) / 2) + $(window).scrollTop() + "px");
+	})
+}
+
+function removeModal(){
+	$("#dialog").fadeOut(300);
+	$("#modalContainer").fadeOut(500, function(){
+		$("#dialog").remove();
+		$("#modalContainer").remove();
+	});
+	
+	$(window).unbind("resize");
+	$(window).unbind("scroll");
+}
+
+
